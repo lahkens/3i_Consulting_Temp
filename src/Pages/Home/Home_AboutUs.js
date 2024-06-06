b4a1b94f66b3bee6fbb6c35ed9e87eb2188da619
Snekhal Prasad <112121062@nitt.edu>
2024-06-06 18:48:54 +0530

    Changes to Home AboutUs Component

--- src/Pages/Home/Home_AboutUs.js
+++ src/Pages/Home/Home_AboutUs.js
@@ -26,30 +26,32 @@ function Home_AboutUs() {
           <li>About Us</li>
         </div>
         <div className="about-us-text">
-          <h1>
-          <span style={{color : '#0D98D9'}}>Digital Transformation</span> with Innovation and Expertise!
-          </h1>
+          <div className = "HeadingText">
+             <h1>
+             <span style={{color : '#0D98D9'}}>Digital Transformation</span> with Innovation and Expertise!
+             </h1>
+          </div>
           <p>Providing innovative, comprehensive digital solutions to drive your organization's transformation. Ensuring success through expertise and cutting-edge technology.</p>
         </div>
         <div className="about-us-services">
           <div className="d-flex align-items-center liDiv1">
-            <img className="mr-3" src={liCheckImg} alt="Check icon"/>
+            <img className="mr-tick" src={liCheckImg} alt="Check icon"/>
             <li>Comprehensive tech services, from app and website development to admin dashboards, tailored to your needs.</li>
           </div>
           <div className="d-flex align-items-center liDiv2">
-            <img className="mr-3" src={liCheckImg} alt="Check icon"/>
+            <img className="mr-tick" src={liCheckImg} alt="Check icon"/>
             <li>We Provide strategic consulting to streamline operations.</li>
           </div>
           <div className="d-flex align-items-center liDiv3">
-            <img className="mr-3" src={liCheckImg} alt="Check icon"/>
+            <img className="mr-tick" src={liCheckImg} alt="Check icon"/>
             <li>Offering secure server deployment and management.</li>
           </div>
           <div className="d-flex align-items-center liDiv4">
-            <img className="mr-3" src={liCheckImg} alt="Check icon"/>
+            <img className="mr-tick" src={liCheckImg} alt="Check icon"/>
             <li>Specializing in SAP, ERP and AWS solutions, optimizing your enterprise resource planning and cloud infrastructure.</li>
           </div>
           <div className="d-flex align-items-start justify-content-center mt-1 liDiv">
-            <img className="mr-3" src={liCheckImg} alt="Check icon"/>
+            <img className="mr-tick" src={liCheckImg} alt="Check icon"/>
             <li>Focusing on client success through personalized service, expert support, and innovative solutions.</li>
             <div>
               <img className="years-img" src={yearsImg} alt="Years of experience"/>
